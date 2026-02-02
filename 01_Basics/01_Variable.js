202c63b99687916a4bbd80ad93f45d4b88598a8d
Shashankkpal <shashankpal079@gmail.com>
2026-02-02 09:07:00 +0000

    container config done

---
+++ 01_Basics/01_Variable.js
@@ -0,0 +1,7 @@
+const accountId=123344
+let accountEmail="Shashank@gmail.com"
+var accountPass="123"
+accountCity="Dehradun"
+let accountState
+accountEmail="hc"
+console.table([accountEmail,accountId,accountPass,accountCity])
